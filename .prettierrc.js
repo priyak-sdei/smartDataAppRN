d45786251440ad3bc8f5fd3edf8e2590b4e474fc
Priya Kumari <priya.kumari@smartData-608.local>
2025-01-29 17:54:33 +0530

    Sonar integration

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,7 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 100, //Specifies the maximum line length before code is wrapped or truncated.
+  tabWidth: 4, // Defines the number of spaces or tabs per indentation level.
+  semi: true, //Controls whether or not semicolons are added at the end of statements.
 };
