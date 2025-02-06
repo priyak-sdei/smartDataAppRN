1d90e903289ed83239694ae282b213d6952d82c3
VaibhavSDM <vaibhavkasana@smartdatainc.net>
2025-02-06 12:17:00 +0530

    redux implementation

---
+++ src/redux/rtkAPI/index.tsx
@@ -0,0 +1,20 @@
+import {fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import {RootState} from '../store';
+
+const createBaseQuery = () => {
+    return fetchBaseQuery({
+        baseUrl: 'http://54.190.192.105:9185/angel/',
+        timeout: 10000,
+        prepareHeaders: async (headers, {getState, endpoint}) => {
+            const state = getState() as RootState;
+            // const token = (state as any)?.auth?.user?.token;
+            // console.log('tokennnnn=====>>', token);
+            // if (token) {
+            //     headers.set('Authorization', `Bearer ${token}`);
+            // }
+            return headers;
+        },
+    });
+};
+
+export {createBaseQuery};
