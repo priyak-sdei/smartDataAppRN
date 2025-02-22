0bdc6b31d60870c0a4531f40e47cedd9b5c72fd8
priyak-sdei <priya.kumari@smartdatainc.net>
2025-02-22 21:50:30 +0530

    Code

--- src/components/common/header/styles.tsx
+++ src/components/common/header/styles.tsx
@@ -1,5 +1,5 @@
 //import {Fonts} from '@theme/typography';
-import {moderateScale, verticalScale} from '@theme/index';
+import {moderateScale, SPACING, verticalScale} from '@theme/index';
 import {Fonts} from '@theme/typography';
 import {StyleSheet} from 'react-native';
 import {ExtendedTheme} from 'src/types/ColorPalette';
@@ -18,34 +18,34 @@ export const createStyles = (colors: ExtendedTheme['colors']) =>
             flex: 1,
             height: verticalScale(60),
             justifyContent: 'space-between',
-            paddingHorizontal: moderateScale(15), // elevation: 3, // Shadow for Android
+            paddingHorizontal: moderateScale(SPACING.s), // elevation: 3, // Shadow for Android
             shadowColor: colors.black, // Shadow for iOS
             shadowOffset: {width: 0, height: 2},
             shadowOpacity: 0.2,
         },
         headerTitle: {
             ...Fonts.bold,
-            fontSize: moderateScale(18),
+            fontSize: moderateScale(SPACING.sm),
         },
         iconContainer: {
             left: 0,
-            width: moderateScale(30),
+            width: moderateScale(SPACING.xl),
         },
         iconGroup: {
             flexDirection: 'row',
-            height: moderateScale(30),
-            width: moderateScale(25),
+            height: moderateScale(SPACING.xl),
+            width: moderateScale(SPACING.l),
         },
 
         imageIcon: {
-            height: moderateScale(28),
+            height: moderateScale(SPACING.xl),
             resizeMode: 'contain',
-            width: moderateScale(28),
+            width: moderateScale(SPACING.xl),
         },
 
         leftJustify: {justifyContent: 'flex-end'},
         titleContainer: {
             alignItems: 'center',
-            flex: 1, // Ensures center alignment,
+            flex: 1,
         },
     });
